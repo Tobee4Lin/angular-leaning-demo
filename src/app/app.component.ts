55881809208e18341ac73451a52e316897c7edc9
zhengzebin <zhengzebin@seerbigdata.com>
2019-11-14 18:49:47 +0800

    add some usages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CountdownTimerComponent } from './CountdownTimer/CountdownTimer.component';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(CountdownTimerComponent, { static: false })
+  private timerComponent: CountdownTimerComponent
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.seconds = () => this.timerComponent.seconds, 0);
+  }
+
+  seconds() {
+    return 0;
+  }
+  start() { this.timerComponent.start(); }
+  stop() { this.timerComponent.stop(); }
+
   title = 'angular-first-demo';
   heroes = [
     {
@@ -20,9 +36,14 @@ export class AppComponent {
   voters = ['mike', 'jack']
   agreed = 0;
   disagreed = 0;
+  currentText = {
+    content: "mmm"
+  }
 
   onVoted(agreed: boolean) {
     agreed ? this.agreed++ : this.disagreed++
   }
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { CountdownTimerComponent } from './CountdownTimer/CountdownTimer.compone
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
